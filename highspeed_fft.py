0a0f160834abd3f0ba990e52869ed212630f13d7
Samvir Thandi <77638166+sthandi1@users.noreply.github.com>
2021-01-25 18:04:37 +0000

    modified edge detection functions

--- highspeed_fft.py
+++ highspeed_fft.py
@@ -8,14 +8,14 @@ Created on Tue Jan 19 19:08:38 2021
 
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.fft import fft, fftfreq, rfft, rfftfreq
+from scipy.fft import rfft, rfftfreq
 import csv
 from statistics import mean
 
 
 def fft_testing(filename):
     """
-    Plotting the csv results from the previous function    
+    Plotting the csv results from the previous function
 
     Returns
     -------
@@ -46,37 +46,35 @@ def fft_testing(filename):
             # append local right edge to global list
             right_edges.append(right_edge)
 
-            
     # converting frames into real time
-    
+
     time = []
-    
+
     for frame in frames:
         time.append(frames[frame]/27000)
-        
+
     # converting pixels to mm edges
-    
+
     mm_left_edge = []
     mm_right_edge = []
- 
-    
+
     for edge in left_edges:
         mm_left_edge.append(left_edges[edge]*0.02)
-    
+
     for edge in right_edges:
         mm_right_edge.append(right_edges[edge]*0.02)
-                        
+
     # jet diameter
     jet_diameter = []
-    
+
     for i in frames:
         jet_diameter.append(0.02*(right_edges[i]-left_edges[i]))
-    
+
     # jet centroid
     jet_centroid = []
     for i in frames:
-        jet_centroid.append(0.02*0.5*(right_edges[i]+left_edges[i]))    
-    
+        jet_centroid.append(0.02*0.5*(right_edges[i]+left_edges[i]))
+
     # setting up plots for left and right edges
     fig, ax = plt.subplots()
     # plotting left edge
@@ -117,7 +115,7 @@ def fft_testing(filename):
     ax4.stem(loc1_diameter_freqs, np.abs(loc1_diameter_fft))
     ax4.set_xlim(2, 500)
     ax4.set_ylim(0, 150)
-    ax4.set_title('Jet diameter change frequencies')
+    ax4.set_title('Jet diameter frequencies')
     ax4.set_xlabel('Frequencies')
     ax4.set_ylabel('Amplitude')
 
@@ -128,19 +126,22 @@ def fft_testing(filename):
         normalised_jet_diameter.append(diameter - jet_diameter_mean)
     fig5, ax5 = plt.subplots()
     ax5.plot(time, normalised_jet_diameter)
-    ax5.set_title("Normalised jet diameter")
+    ax5.set_title("Shifted jet diameter")
     ax5.set_xlabel('Time (seconds)')
     ax5.set_ylabel('Normalised jet diameter (mm)')
 
     # normalisation fft for jet diameter
-    
+
     normalised_jet_diameter_fft = rfft(normalised_jet_diameter)
-    normalised_jet_diameter_freqs = rfftfreq(len(normalised_jet_diameter), 1/27000)
+    normalised_jet_diameter_freqs = rfftfreq(len(normalised_jet_diameter),
+                                             1/27000)
     fig6, ax6 = plt.subplots()
-    ax6.stem(normalised_jet_diameter_freqs, np.abs(normalised_jet_diameter_fft))
-    ax6.set_title('Normalised jet diameter FFT')
+    ax6.stem(normalised_jet_diameter_freqs,
+             np.abs(normalised_jet_diameter_fft))
+    ax6.set_title('Shifted jet diameter FFT')
     ax6.set_xlabel('Frequencies')
     ax6.set_ylabel('Amplitude')
+    ax6.set_xlim(0, 500)
 
     # trying normalisation for jet centroid
     normalised_jet_centroid = []
@@ -149,27 +150,28 @@ def fft_testing(filename):
         normalised_jet_centroid.append(centroid - jet_centroid_mean)
     fig7, ax7 = plt.subplots()
     ax7.plot(time, normalised_jet_centroid)
-    ax7.set_title("Normalised jet centroid")
+    ax7.set_title("Shifted jet centroid")
     ax7.set_xlabel('Time (seconds)')
     ax7.set_ylabel('Normalised jet centroid (mm)')
 
     # normalisation fft for jet centroid
-    
+
     normalised_jet_centroid_fft = rfft(normalised_jet_centroid)
-    normalised_jet_centroid_freqs = rfftfreq(len(normalised_jet_centroid), 1/27000)
+    normalised_jet_centroid_freqs = rfftfreq(len(normalised_jet_centroid),
+                                             1/27000)
     fig8, ax8 = plt.subplots()
-    ax8.stem(normalised_jet_centroid_freqs, np.abs(normalised_jet_centroid_fft))
-    ax8.set_title('Normalised jet centroid FFT')
+    ax8.stem(normalised_jet_centroid_freqs,
+             np.abs(normalised_jet_centroid_fft))
+    ax8.set_title('Shifted jet centroid FFT')
     ax8.set_xlabel('Frequencies')
     ax8.set_ylabel('Amplitude')
     ax8.set_xlim(0, 250)
 
+    print(len(normalised_jet_centroid_freqs))
     # identifying peak frequencies
     # jet diameter
 
-    for i in range(len(np.abs(normalised_jet_diameter_fft))):
-        if np.abs(normalised_jet_diameter_fft)[i] > 100:
-            print("Amplitudes:",np.abs(normalised_jet_diameter_fft)[i])
-            print("Frequencies:", normalised_jet_centroid_freqs[i])
-
-
+    # for i in range(len(np.abs(normalised_jet_diameter_fft))):
+    #     if np.abs(normalised_jet_diameter_fft)[i] > 100:
+    #         print("Amplitudes:",np.abs(normalised_jet_diameter_fft)[i])
+    #         print("Frequencies:", normalised_jet_centroid_freqs[i])
--- highspeed_image_processing.py
+++ highspeed_image_processing.py
@@ -10,15 +10,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 from mraw_v2 import mraw
 import csv
-from scipy.fft import fft, fftfreq
 
 
-def nothing(x):
+def change(x):
     # this function is used by the trackbars
-    print(x)
+    pass
 
 
-def single_image_thresh_value(inputFile, thresh):
+def single_image_thresh_value(inputFile):
     """
     This function is used to determine the threshold value to be used in the
     processing
@@ -36,28 +35,28 @@ def single_image_thresh_value(inputFile, thresh):
     # third argument is the starting value
     # fourth argument is the maximum value
     # fifth argument is the callback function
-    cv2.createTrackbar('threshold', 'threshold trackbar', 0, 4096, nothing)
+    cv2.createTrackbar('threshold', 'threshold trackbar', 0, 4096, change)
 
     movie = mraw(inputFile)
     image = movie[0]
     # while loop allows image to be dynamically updated
-    while True:
+    while(True):
         # assign the threshold value to a parameter
         thresh = cv2.getTrackbarPos('threshold', 'threshold trackbar')
         # apply binary threshold where below threshold is zero and above is max
         _, th1 = cv2.threshold(image, thresh, 4096, cv2.THRESH_BINARY)
         # show thresholded image
-        cv2.imshow('threshold trackbar', th1)
+        cv2.imshow('thresh', th1)
         # how long to wait before closing window
-        key = cv2.waitKey(1)
-        if key == 27:
+        if cv2.waitKey(1) == 27:
             break
         # stops crashes and destroys windows
     cv2.destroyAllWindows()
 
 
 
-def single_image_thresh_data(inputFile, thresh, y_location):
+
+def single_image_thresh_data(inputFile, thresh, z_location):
     """
     This function gets the data for a single image and plots it on a graph
     it compares the normal image and thresholded image
@@ -86,32 +85,32 @@ def single_image_thresh_data(inputFile, thresh, y_location):
     # LEFT EDGES
     for pixel in range(width):
         # for loop goes through the threshold array at the given
-        # downstream 'y' position (y_location) and then cycles across in the 'x'
-        # direction until it finds a zero. This zero indicate the edge of
+        # downstream 'z' position (z_location) and then cycles across in the
+        # 'x' direction until it finds a zero. This zero indicate the edge of
         # the jet.
-        if th1[y_location, pixel] == 0:
+        if th1[z_location, pixel] == 0:
             # assign the iterator to a variable
             left_edge = pixel
             # exit the for loop once edge has been found
             break
 
-
     # RIGHT EDGES
     for pixel in range(width):
         # invert the loop so it counts down instead of up
         inv = width - pixel - 1
-        # This for loop finds the rihgt edge at y_location and assings it to a
+        # This for loop finds the rihgt edge at z_location and assings it to a
         # variable
-        if th1[y_location, inv] == 0:
+        if th1[z_location, inv] == 0:
             # assigning the pixel to a variable
             right_edge = inv
             # exit the for loop once edge has been found
             break
-    
+
     # plot the left edges point on the thresholded image
-    ax1.plot(left_edge, y_location, linestyle='none', marker='x', markersize=12)
+    ax1.plot(left_edge, z_location, linestyle='none', marker='x',
+             markersize=12)
     # plt the right edges point on the thresholded image
-    ax1.plot(right_edge, y_location, linestyle='none', marker='x',
+    ax1.plot(right_edge, z_location, linestyle='none', marker='x',
              markersize=12)
     fig.set_size_inches(4, 8)
     fig1.set_size_inches(4, 8)
@@ -120,11 +119,12 @@ def single_image_thresh_data(inputFile, thresh, y_location):
     fig1.savefig(fname='thresholded_edges.png', format='png')
     fig2.savefig(fname='thresholded.png', format='png')
 
-def multi_image(y_loc, inputFile, thresh):
+
+def multi_image(z_locations, inputFile, thresh):
     """
     This is the main code. It runs through all the photos in the file to
-    determine edge locations and then it saves it in a csv file to be 
-    processed later
+    determine edge locations and then it saves it in a csv file to be
+    processed later. It takes a list or numpy array of z locations
 
     Returns
     -------
@@ -138,8 +138,8 @@ def multi_image(y_loc, inputFile, thresh):
     # defines the numnber of images in the movie
     frames = movie.image_count
     print("Number of images in movie: {:d}".format(frames))
-    # set up a storage arrays which has the same number of rows as the number of
-    # images in the movie (frames) and 3 columns. First column is the frame
+    # set up a storage arrays which has the same number of rows as the number
+    # of images in the movie (frames) and 3 columns. First column is the frame
     # number, second column is the left edge, third column is the right edge
 
     edges = np.zeros((frames, 3))
@@ -153,47 +153,60 @@ def multi_image(y_loc, inputFile, thresh):
         # iterator
         image = movie[frame]
         # define the frame number in the storage array first column. The frame
-        # is the y location in the storage array and also the value. Frame is
+        # is the z location in the storage array and also the value. Frame is
         # the iterator too. e.g. edges[frame, 0] = frame will give the first
         # column value and then the row number is the frame number. This
         # is given the value of the frame. Therefore the array of zeros
         # becomes an array where the first column counts up from 0 to
         # the number of frames in the video.
         edges[frame, 0] = frame
-       
+
         # apply binary threshold where below threshold is zero and above is
         # 4096 which is 2^12 since the image is a 12 bit image
         _, th1 = cv2.threshold(image, thresh, 4096, cv2.THRESH_BINARY)
 
-        # LEFT EDGES
-        for pixel in range(width):
-            # for loop goes through the threshold array at the given
-            # downstream 'y' position (y_loc) and then cycles across in the 'x'
-            # direction until it finds a zero. This zero indicate the edge of
-            # the jet.
+        edges_zloc0 = edge_detector(edges, z_loc[0], frame, th1, width)
+        edges_zloc1 = edge_detector(edges, z_loc[0], frame, th1, width)
+        edges_zloc2 = edge_detector(edges, z_loc[0], frame, th1, width)
+        edges_zloc3 = edge_detector(edges, z_loc[0], frame, th1, width)
+        edges_zloc4 = edge_detector(edges, z_loc[0], frame, th1, width)
+        edges_zloc5 = edge_detector(edges, z_loc[0], frame, th1, width)
+        edges_zloc6 = edge_detector(edges, z_loc[0], frame, th1, width)
+        edges_zloc7 = edge_detector(edges, z_loc[0], frame, th1, width)
+        edges_zloc8 = edge_detector(edges, z_loc[0], frame, th1, width)
+        edges_zloc9 = edge_detector(edges, z_loc[0], frame, th1, width)
 
-            # This for loop finds the left edge at y_loc and assigns it to the
-            # second column, which is indexed as 1
-            if th1[y_loc, pixel] == 0:
-                edges[frame, 1] = pixel
-                # exit the for loop once edge has been found
-                break
-    
-        # RIGHT EDGES
-        for pixel in range(width):
-            # invert the loop so it counts down instead of up
-            inv = width - pixel - 1
-            # This for loop finds the right edge at y_loc and assigns it to the
-            # third column, which is indexed as 2
-            if th1[y_loc, inv] == 0:
-                # assigning the pixel to the storage array for the given frame
-                edges[frame, 2] = inv
-                # exit the for loop once edge has been found
-                break
-    
+    for z_loc in z_locations:
+        output_filename = 'edges_results_' + str(z_loc) + '.csv'
+        # save the edges storage matrix into a text file
+        np.savetxt(output_filename, edges, fmt='%d', delimiter=',')
 
 
-    filename = 'edges_results_' + str(y_loc) + '.csv'
+def edge_detector(edges, z_loc, frame, th1, width):
+    for pixel in range(width):
+        # for loop goes through the threshold array at the given
+        # downstream 'z' position (z_loc) and then cycles across in the 'x'
+        # direction until it finds a zero. This zero indicate the edge of
+        # the jet.
 
-    # save the edges storage matrix into a text file
-    np.savetxt(filename, edges, fmt='%d', delimiter=',')
+        # This for loop finds the left edge at z_loc and assigns it to the
+        # second column, which is indexed as 1
+        if th1[z_loc, pixel] == 0:
+            edges[frame, 1] = pixel
+            # exit the for loop once edge has been found
+            break
+
+    # RIGHT EDGES
+    for pixel in range(width):
+        # invert the loop so it counts down instead of up
+        inv = width - pixel - 1
+        # This for loop finds the right edge at z_loc and assigns it to the
+        # third column, which is indexed as 2
+        if th1[z_loc, inv] == 0:
+            # assigning the pixel to the storage array for the given frame
+            edges[frame, 2] = inv
+            # exit the for loop once edge has been found
+            break
+    return edges
+
+single_image_thresh_value("/Volumes/My Passport/Experiments/2020-02 High speed camera experiments/0_cowl/Re_1551/We_5_22/0_cowl_1551_522.cihx")
