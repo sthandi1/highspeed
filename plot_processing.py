aab77e46c85febd5c8e843e618c7035f40fa2c73
Samvir Thandi <77638166+sthandi1@users.noreply.github.com>
2021-06-24 15:29:40 +0100

    readded the mark every to moro data

--- plot_processing.py
+++ plot_processing.py
@@ -580,11 +580,11 @@ def morozumi_comparison(morozumi_axi_5_22, morozumi_as_5_22,
     ax.plot(freqs_22_9, savgol_axi_22_9, label='22.9 Experimental data',
             marker='s', markevery=100, markersize=8, color='black')
     ax.plot(morozumi_axi_5_22_freqs, moro_axi_5_22_growth_rates,
-            label='Morozumi and Fukai 5.22 data', marker='o', markersize=8,
-            color='black', linestyle='dashed')
+            label='Morozumi and Fukai 5.22 data', marker='o', markevery=100,
+            markersize=8, color='black', linestyle='dashed')
     ax.plot(morozumi_axi_22_9_freqs, moro_axi_22_9_growth_rates,
-            label='Morozumi and Fukai 22.9 data', marker='s', markersize=8,
-            color='black', linestyle='dashed')
+            label='Morozumi and Fukai 22.9 data', marker='s', markevery=100,
+            markersize=8, color='black', linestyle='dashed')
     ax.set_xlim(0, 800)
     ax.set_ylim(0, 200)
     ax.legend()
